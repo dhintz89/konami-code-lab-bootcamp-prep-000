781a7a36c284a038d8623fe2edc9cc51c376c0a2
Dan Hintz <dhintz@live.com>
2018-07-28 18:32:58 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,16 +11,14 @@ const codes = [
   "a"
 ];
 
-var body = document.querySelector('body');
-
 let index = 0;
 
 function init() {
   const body = document.querySelector('body');
   body.addEventListener('keydown', function(e) {
     const key = e.key;
-    if(key ===codes[index]) {
-      index++
+    if(key === codes[index]) {
+      index++;
       if(index === codes.length) {
         alert("Congratulations, you've entered the Konami Code");
         index = 0;
@@ -28,5 +26,5 @@ function init() {
     }else{
       index=0;
     }
-  }
+  });
 }
